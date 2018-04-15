8251b3a8f89d68a61f4b58fa169bc1bfc8b0c4bc
RRRLuxoft <rrr.lands@gmail.com>
2018-04-15 12:23:48 +0200

    eureka config

---
+++ reservation-business-services/src/main/java/home/investigation/rrr/business/reservation/domain/Room.java
@@ -0,0 +1,17 @@
+package home.investigation.rrr.business.reservation.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Date: 09.04.2018
+ */
+@Getter
+@Setter
+public class Room {
+    private long id;
+    private String name;
+    private String roomNumber;
+    private String bedInfo;
+
+}
