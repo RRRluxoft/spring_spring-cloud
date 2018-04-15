a8d8e2cd736b490ad587b5aacf6ff32865191845
RRRLuxoft <rrr.lands@gmail.com>
2018-04-15 22:51:25 +0200

    eureka configuration - http://localhost:8500/rooms

---
+++ reservation-business-services/src/main/java/home/investigation/rrr/services/business/reservation/RoomReservationController.java
@@ -0,0 +1,30 @@
+package home.investigation.rrr.services.business.reservation;
+
+import home.investigation.rrr.services.business.reservation.domain.Room;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+public class RoomReservationController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/rooms")
+    public List<Room> getAllRooms() {
+        ResponseEntity<List<Room>> roomsResponse = this.restTemplate.exchange(
+            "http://ROOMSERVICES/rooms", HttpMethod.GET, null,
+            new ParameterizedTypeReference<List<Room>>() {
+            }
+        );
+        return roomsResponse.getBody();
+    }
+
+}
